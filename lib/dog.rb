cf944d937c4dc88e839863226107cd21a362256b
Kat Maldon <katharinamaldon@gmail.com>
2019-02-01 18:40:40 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,19 +5,19 @@ class Dog
     @breed = breed 
   end 
   
-  #def name=(name)
-    #@name = name 
-  #end 
+  def name=(name)
+    @name = name 
+  end 
   
-  #def breed=(breed = "Mutt")
-   # @breed = breed
-#  end 
+  def breed=(breed = "Mutt")
+    @breed = breed
+  end 
   
- # def name
-  #  @name
-#  end 
+  def name
+    @name
+  end 
   
- # def breed 
-  #  @breed 
-  #end 
+  def breed 
+    @breed 
+  end 
 end 
