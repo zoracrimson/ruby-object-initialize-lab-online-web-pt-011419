6769058caa9f23449df59d6a86bebcbda46dd84f
Kat Maldon <katharinamaldon@gmail.com>
2019-02-01 18:22:41 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,23 @@
+class Dog 
+  
+  def initilize(name, breed = "Mutt")
+    @name = name 
+    @breed = breed 
+  end 
+  
+  def name=(name)
+    @name = name 
+  end 
+  
+  def breed=(breed = "Mutt")
+    @breed = breed
+  end 
+  
+  def name
+    @name
+  end 
+  
+  def breed 
+    @breed 
+  end 
+end 
